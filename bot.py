68c7abb7ac06b2722b0eb9623e35dd31fb0f0917
suimonIT <esecefe7@gmail.com>
2026-03-29 19:37:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ TOKEN = "8429890592:AAHkdeR_2pGp4EOVTT-lBrYAlBlRjK2tW7Y"
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_FILE = os.path.join(BASE_DIR, "players.json")
-ALLOWED_GROUP_IDS = {-1002664937769, -1003839722848}
+ALLOWED_GROUP_IDS = {-1002664937769, -1003407035529}
 # Only these user IDs + the Telegram group owner can use privileged admin commands
 PRIVILEGED_USER_IDS = {1638084297, 7105730933}
 MENU_IMAGE_CANDIDATES = ("logo.JPG", "logo.jpg", "logo.png", "menu.jpg", "menu.png")
